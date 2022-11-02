1afd8ed359986ae736dc60d6f7b419c83671e236
PedroasoLuiz <pedroluiz9922@gmail.com>
2022-11-02 10:18:00 -0300

    Instalação pacote MetroFramework

--- LCD-bidding/Projeto01/Form1.cs
+++ LCD-bidding/Projeto01/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Projeto01
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroFramework.Forms.MetroForm
     {
         public Form1()
         {
