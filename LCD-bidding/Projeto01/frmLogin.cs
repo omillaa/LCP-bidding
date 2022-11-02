c3f097eb94a09cda7fb021f6784b43f849f0c647
PedroasoLuiz <pedroluiz9922@gmail.com>
2022-11-02 11:53:57 -0300

    updateFRMLogin
    02-11-2022

--- LCD-bidding/Projeto01/frmLogin.cs
+++ LCD-bidding/Projeto01/frmLogin.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Projeto01
 {
-    public partial class Form1 : MetroFramework.Forms.MetroForm
+    public partial class frmLogin : Form
     {
-        public Form1()
+        public frmLogin()
         {
             InitializeComponent();
         }
